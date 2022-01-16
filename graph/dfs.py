6973a52617459bf9b09fac6af4e9e0927842cda3
Pranav Kumar <pbpranav24@gmail.com>
2022-01-16 13:27:05 +0530

    construction and search in graph

---
+++ graph/dfs.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self,u,v):
+        self.graph[u].append(v)
+
+    def DFSUtil(self, v, visited):
+        visited.add(v)
+        print(v, end=" ")
+
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    def DFS(self, v):
+        visited = set()
+        self.DFSUtil(v, visited)
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("Following is DFS from (starting from vertex 2)")
+g.DFS(2)
